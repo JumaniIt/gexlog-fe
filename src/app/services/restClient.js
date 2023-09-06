a27b0985d15cecca183d30ed161f714afd2d9b73
Nei <nicolas.ortola@mercadolibre.com>
2023-09-06 11:39:57 -0300

    log base url

--- src/app/services/restClient.js
+++ src/app/services/restClient.js
@@ -9,6 +9,7 @@ const internalError = {
 export const post = async (uri, req) => {
   try {
     console.log("BASE URL: " + BASE_URL)
+    
     const response = await fetch(BASE_URL + uri, {
       method: "POST",
       headers: {
