cc1dd93572660f3a3712b7c03c82549fdc7ecf18
Nei <nicolas.ortola@mercadolibre.com>
2023-10-06 09:46:09 -0300

    temp cookie

--- src/app/services/restClient.js
+++ src/app/services/restClient.js
@@ -33,9 +33,9 @@ const perform = async (
     }
   });
 
-  // const jwtToken = getCookie("jwtTokenTemp");
+  const jwtToken = getCookie("jwtTokenTemp");
   const headers = new Headers();
-  // headers.append("x-auth-token", "Bearer " + jwtToken);
+  headers.append("x-auth-token", "Bearer " + jwtToken);
   headers.append("x-auth-origin", "gexlog-fe");
   
 
